7a40064115f89eff10ae9c95e2321bc01162019c
xiaosansiji <xiaosansiji@gmail.com>
2021-12-20 17:11:33 +0800

    feat: add controlled demo

---
+++ examples/date-picker/demos/base-controlled.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tdesign-demo-item--datepicker">
+    <t-date-picker mode="date" :value="value" @change="handleChange" />
+  </div>
+</template>
+
+<script lang="jsx">
+import dayjs from 'dayjs';
+
+export default {
+  data() {
+    return {
+      value: dayjs().toISOString(),
+    };
+  },
+  methods: {
+    handleChange(value) {
+      this.value = value;
+    },
+  },
+};
+</script>
